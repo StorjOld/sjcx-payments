5b773ee3b4836a36979bb48400c641dbf987114e
Andrew Kim <andrew.kim.h@gmail.com>
2015-11-11 16:22:39 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='sjcx-payments', version='1.0',
     description = 'sjcx rewards calculator',
-    author = 'Andrew Kim', author_email='shawn@storj.io',
+    author = 'Andrew Kim',
     url='http://storj.io',
     install_requires=['xlrd', 'requests', 'pymongo']
     tests_require=['coverage', 'coveralls'],
