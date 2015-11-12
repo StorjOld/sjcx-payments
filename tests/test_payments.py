2a3182dd21972d0bbc3dd1b940f5ae17dee6cde7
andrew kim <andrew.kim.h@gmail.com>
2015-11-12 15:38:19 -0500

    added tests

--- tests/test_payments.py
+++ tests/test_payments.py
@@ -28,6 +28,32 @@ class Payments(unittest.TestCase):
         self.assertTrue(points > 0)
         conn.close()
 
+    def test_distribute_sjcx(self):
+        conn = sqlite3.connect('data/test_rewards.db')
+        cursor = conn.cursor()
+        test_date = dt.datetime(2015, 11, 1, 0, 0, 0)
+        payments.distribute_sjcx(conn, cursor, test_date)
+        cursor.execute('''SELECT SUM(sjcx_reward) FROM rewards WHERE
+                          payment_date = ?''', (str(test_date),))
+        total = cursor.fetchone()[0]
+        self.assertEqual(total, 100000)
+        conn.close()
+
+    def test_init_past_rewards(self):
+        conn = sqlite3.connect('data/test_rewards.db')
+        cursor = conn.cursor()
+        payments.init_past_rewards(conn, cursor)
+        test_address = '19cBuNFYFdM9C8RB6bfnhM78RKaHHDVHFm'
+        test_total = 192.42
+        cursor.execute('''SELECT payout_address WHERE payout_address = ? AND
+                       total_usd_reward = ?''', (str(test_address), test_total))
+        data = cursor.fetchall()
+        self.assertFalse(len(data == 0))
+
+    def test_create_whitelist(self):
+        whitelist = payments.create_whitelist()
+        self.assert(len(whitelist) > 0)
+
     def test_uptime_logistic_function(self):
         uptime = 0.5
         value = payments.uptime_logistic_function(uptime)
